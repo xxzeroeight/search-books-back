ea78bbfa478cfde194f6cc4c99cb1b00f58f10a6
xxzeroeight <xxzeroeight@naver.com>
2025-07-20 14:46:44 +0900

    refactor: cors 추가

--- src/main/java/com/search/books/global/config/WebConfig.java
+++ src/main/java/com/search/books/global/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://search-isbn.web.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
